f2635436eed4cd0d4d94071db0b872592ed88c97
Ignacio Furió José <bichoifj@gmail.com>
2023-02-25 17:30:54 +0100

    feat: server runs ok, modules role and user

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const db = require('./db/db');
+const PORT = 3000;
+const { sequelize } = require("./models/index");
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server ready at port ${PORT}`)
+
+})
---
+++ models/user.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    nombrr_usuario: DataTypes.STRING,
+    nombre: DataTypes.STRING,
+    email: DataTypes.STRING,
+    email: DataTypes.STRING,
+    contraseña: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
